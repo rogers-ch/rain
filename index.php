81c35922e9b339c41a94c9e30e384bcd200311b3
Corey Rogers <crogers25@mail.greenriver.edu>
2020-04-22 17:18:36 -0400

    Home page view added

--- index.php
+++ index.php
@@ -10,14 +10,14 @@ require_once('vendor/autoload.php');
 //Instantiate the F3 Base class
 $f3 = Base::instance();   //Base is a class; instance() is a static method in the Base class
                           // this becomes the object we use to run fatfree
-                          //PHP uses colon to call static methods
+                          //PHP uses double colon to call static methods
 
 //Define a default route
 $f3->route('GET /', function() {
-    echo '<h1>It is raining today</h1>';
+    //echo '<h1>It is raining today</h1>';
 
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //Run F3
